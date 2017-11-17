8f6d9f9550362118d2ef548bc2b1aa75d6e91245
Raymond Feliciano <raymond.feliciano2011@gmail.com>
2017-11-16 22:22:02 -0500

    Test Commit

--- Stack Skills/ViewController.swift
+++ Stack Skills/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count = 1
 
+    @IBOutlet weak var yoMammaLabel: UILabel!
+    
+    @IBAction func squishMeTapped(sender: AnyObject) {
+        if (count >= 10) {
+            yoMammaLabel.text = "you tapped the button " + String(count) + " times"
+        }
+        count += 1
+        print("test button")
+    }
+    
+    @IBAction func anotherButtonTapped(sender: AnyObject) {
+        yoMammaLabel.text = "This guy is lame"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        yoMammaLabel.text = "Your Fucking Moma"
     }
 
     override func didReceiveMemoryWarning() {
